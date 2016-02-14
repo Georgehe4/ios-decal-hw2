1c665624b325dbf3a84ae02cca20cbfe279bd716
George He <georgehe@berkeley.edu>
2016-02-13 23:36:06 -0800

    Hw2 done

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -12,8 +12,17 @@ class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
     
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var returnButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var xButton: UIButton!
+    @IBOutlet weak var yButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var downButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var upButton: UIButton!
     var keyboardView: UIView!
-
     override func updateViewConstraints() {
         super.updateViewConstraints()
     
@@ -25,6 +34,47 @@ class KeyboardViewController: UIInputViewController {
         loadInterface()
     }
 
+    func aTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("A")
+    }
+    func bTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("B")
+    }
+    func xTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("X")
+    }
+    func yTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("Y")
+    }
+    func upTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("↑")
+    }
+    func downTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("↓")
+    }
+    func leftTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("←")
+    }
+    func rightTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("→")
+    }
+    func returnTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("\n")
+    }
+    func deleteTap() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.deleteBackward()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated
@@ -45,8 +95,19 @@ class KeyboardViewController: UIInputViewController {
         view.addSubview(keyboardView)
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
+        aButton.addTarget(self, action: "aTap", forControlEvents: .TouchUpInside)
+        bButton.addTarget(self, action: "bTap", forControlEvents: .TouchUpInside)
+        xButton.addTarget(self, action: "xTap", forControlEvents: .TouchUpInside)
+        yButton.addTarget(self, action: "yTap", forControlEvents: .TouchUpInside)
+        upButton.addTarget(self, action: "upTap", forControlEvents: .TouchUpInside)
+        downButton.addTarget(self, action: "downTap", forControlEvents: .TouchUpInside)
+        leftButton.addTarget(self, action: "leftTap", forControlEvents: .TouchUpInside)
+        rightButton.addTarget(self, action: "rightTap", forControlEvents: .TouchUpInside)
+        deleteButton.addTarget(self, action: "deleteTap", forControlEvents: .TouchUpInside)
+        returnButton.addTarget(self, action: "returnTap", forControlEvents: .TouchUpInside)
         
-    }
+
+}
 
 
 }
